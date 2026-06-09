d267862255eae947a84fdfaf5de881d76f4e9ba1
tqdiep <trinhdiep.zoro@gmail.com>
2026-06-09 15:50:26 +0700

    change path script buils-jitsi.sh

--- build-jitsi.sh
+++ build-jitsi.sh
@@ -3,20 +3,20 @@
 set -e
 
 echo "=== Bước 1: Cài dependencies cho jitsi-meet ==="
-cd ~/jitsi-meet
+cd ~/jitsi-meet-build/jitsi-meet
 npm install
 
 echo "=== Bước 2: Link lib-jitsi-meet local ==="
-sudo rm -rf ~/jitsi-meet/node_modules/lib-jitsi-meet
-ln -sfn ~/lib-jitsi-meet ~/jitsi-meet/node_modules/lib-jitsi-meet
+sudo rm -rf ~/jitsi-meet-build/jitsi-meet/node_modules/lib-jitsi-meet
+ln -sfn ~/jitsi-meet-build/lib-jitsi-meet ~/jitsi-meet-build/jitsi-meet/node_modules/lib-jitsi-meet
 
 echo "=== Bước 3: Build lib-jitsi-meet ==="
-cd ~/lib-jitsi-meet
+cd ~/jitsi-meet-build/lib-jitsi-meet
 npm update
 npm run build
 
 echo "=== Bước 4: Build jitsi-meet ==="
-cd ~/jitsi-meet
+cd ~/jitsi-meet-build/jitsi-meet
 make
 
 echo "=== Bước 5: Build package DEB ==="
